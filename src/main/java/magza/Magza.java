743045e1b081ae43f2d4caf3869ec3ab9ac51821
OzlemHaslak <ozlemhaslak@gmail.com>
2023-02-23 15:59:34 +0100

    v3

--- src/main/java/magza/Magza.java
+++ src/main/java/magza/Magza.java
@@ -4,6 +4,7 @@ public class Magza {
     public static void main(String[] args) {
         System.out.println("v1");
         System.out.println("v2");
+        System.out.println("v3");
 
     }
 }
